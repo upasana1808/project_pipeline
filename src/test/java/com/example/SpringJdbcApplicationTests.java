6c3eb38d5d9300242893c9680e345a80e40c7ae7
upasana1808 <upasana.dewangan@gmail.com>
2026-07-29 14:33:04 +0530

    test

--- src/test/java/com/example/SpringJdbcApplicationTests.java
+++ src/test/java/com/example/SpringJdbcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringJdbcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
